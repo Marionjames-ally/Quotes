575765aa6f95633eafe9a80796a8cd861dcc054e
Marionjames-ally <marionjames2018@gmail.com>
2020-01-13 13:17:06 +0300

    modify quote-details

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,8 +5,7 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) { }
-
-  // @HostListener()
-
-}
+  constructor(private elem:ElementRef) { 
+    this.elem.nativeElement.style.background="red"
+  }
+ }
